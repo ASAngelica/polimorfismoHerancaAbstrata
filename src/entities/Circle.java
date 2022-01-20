825a87d1d740efd40e50ba8227c3af23e4763251
ASAngelica <asa196549@gmail.com>
2022-01-20 19:53:51 -0300

    Projeto criado

---
+++ src/entities/Circle.java
@@ -0,0 +1,32 @@
+package entities;
+
+import entities.enums.Color;
+
+public class Circle extends Shape {
+	
+	public static final double PI = 3.14159;
+	
+	private Double radius;
+	
+	
+
+	public Circle(Color color, Double radius) {
+		super(color);
+		this.radius = radius;
+	}	
+
+
+	public Double getRadius() {
+		return radius;
+	}
+	
+	public void setRadius(Double radius) {
+		this.radius = radius;
+	}
+	
+	@Override
+	public Double area() {
+		return PI * Math.pow(radius, 2);
+	}
+
+}
